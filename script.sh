fbaee0fe78853bab9137090d7311fe65ca42fa6b
seshasaisura <51076882+seshasaisura@users.noreply.github.com>
2019-05-28 14:39:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,3 +11,4 @@ echo "$Environment for $Variable" >> test
 echo "[INFO] GIT Jenkins Integratioon Demo"
 echo "[INFO] POLL SCM Demo"
 echo "[INFO] uDeploy GIT Integration Demo"
+echo "[INFO] uDeploy GIT Integration Demo1"
